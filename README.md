48a7e24b4f007eb00f2d90ed1880366e57def5e5
kvssankar <55633494+kvssankar@users.noreply.github.com>
2020-07-26 13:21:56 +0530

    fix

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# hit-npm
+# Git ultimate cloner
+
 Are you tried of clone repo, opening in vs-code and installing packages... No more use hit and make your life simple
+This package clone the repo, installs packages required from package.json and instantly opens vscode for you
+
+## Usage
+
+    npm i -g git-ultimate-cloner
+    cd directory
+    lit https://hithub.com/user/repolink
+
+## Prerequsites
+
+1.  node installed
+2.  vs code installed
+3.  git installed globally
---
+++ index.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+let shell = require("shelljs");
+let colors = require("colors");
+
+let url = process.argv[2];
+
+let names = [];
+
+function reverseString(str) {
+  return str.reverse().join("");
+}
+
+for (let i = url.length - 5; i > 0; i--) {
+  if (url[i] === "/") break;
+  names.push(url[i]);
+}
+
+let app = reverseString(names);
+
+const run = async () => {
+  await clone();
+  await cd();
+  await open();
+  shell.exit(200);
+};
+
+const clone = () => {
+  return new Promise((resolve) => {
+    console.log(`\n ${app} is being cloned\n`.cyan);
+    shell.exec(`git clone ${url}`, () => {
+      console.log(`\n ${app} has been cloned\n`.blue);
+      resolve();
+    });
+  });
+};
+
+const cd = () => {
+  return new Promise((resolve) => {
+    console.log(`\n npm packages being installed \n`.cyan);
+    shell.exec(`cd ${app} && npm i`, () => {
+      console.log("\n npm packages installed\n".blue);
+      resolve();
+    });
+  });
+};
+
+const open = () => {
+  return new Promise((resolve) => {
+    console.log(`\n opening vs code ... \n`.cyan);
+    shell.exec(`cd ${app} && code .`, () => {
+      console.log("\n opened vs code\n".blue);
+      resolve();
+    });
+  });
+};
+
+run();
