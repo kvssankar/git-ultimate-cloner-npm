5ceaf6193517a81644f80359314db2fcb09140c3
Tanmay000009 <tanmayvyas009@gmail.com>
2021-08-29 17:29:19 +0530

    Custom folder name feature added
    Invalid link bug fixed

--- index.js
+++ index.js
@@ -39,7 +39,8 @@ const run = async () => {
 const check = () => {
   return new Promise((resolve) => {
     if (!url.includes("https://github.com/")) {
-      shell.echo("\nEnter a valid github url\n".red);
+      shell.echo("\nError: Enter a valid github url\n".red);
+      shell.exit(1);
     }
     if (!shell.which("git")) {
       shell.echo(
