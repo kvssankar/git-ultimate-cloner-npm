26e1bfd8e40ed1b1aa776d90cb9355cd7e520f7b
Tanmay000009 <tanmayvyas009@gmail.com>
2021-08-29 17:04:30 +0530

    Custom folder name feature added

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ var depSpin = new Spinner("%s");
 depSpin.setSpinnerString("|/-\\");
 
 let url = process.argv[2];
-
+let foldername = process.argv[3];
 let app;
-
 
 
   temp_str = url.replace("https://github.com/","");
@@ -22,6 +21,7 @@ let app;
   temp_str = temp_str.slice(t1);
   temp_str.replace("/","");
   app = temp_str;
+
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
@@ -57,34 +57,62 @@ const clone = () => {
   return new Promise((resolve) => {
     shell.echo("\nAll checks passed, Let the fun begin\n".rainbow);
     console.log(`\n ${capitalizeFirstLetter(app)} is being cloned...\n`.cyan);
-    depSpin.start();
-    shell.exec(`git clone ${url}`, () => {
-      depSpin.stop();
-      console.log(
-        `\n\n ${capitalizeFirstLetter(app)} has been cloned successfully\n`
-          .green
-      );
+
+    if (!foldername) {
+      depSpin.start();
+      shell.exec(`git clone ${url}`, () => {
+        depSpin.stop();
+        console.log(
+          `\n\n ${capitalizeFirstLetter(app)} has been cloned successfully\n`
+            .green
+        );
       resolve();
-    });
+      });
+    } else {
+      depSpin.start();
+      shell.exec(`git clone ${url}`, () => {
+        depSpin.stop();
+        console.log(
+          `\n\n ${capitalizeFirstLetter(app)} has been cloned successfully\n`
+            .green
+        );
+      shell.exec(`mv ${app} ${foldername}`);
+      resolve();
+      });
+    }
   });
 };
 
 const cd = () => {
   return new Promise((resolve) => {
-    shell.cd(`${app}`);
-    if (!shell.test("-f", "package.json")) {
+    if (foldername) {
+      shell.cd(`${foldername}`);
+    } else {
+      shell.cd(`${app}`);
+    }
+      if (!shell.test("-f", "package.json")) {
       console.log(`\n Can't find package.json in the root directory \n`.yellow);
       shell.cd("..");
       resolve();
     } else {
       shell.cd("..");
       console.log("\n Npm packages are being installed...".yellow);
-      depSpin.start();
-      shell.exec(`cd ${app} && npm install`, () => {
-        depSpin.stop();
-        console.log("\n Npm packages got installed\n".green);
-        resolve();
-      });
+
+      if (!foldername) {
+        depSpin.start();
+        shell.exec(`cd ${app} && npm install`, () => {
+          depSpin.stop();
+          console.log("\n Npm packages got installed\n".green);
+          resolve();
+        });
+      } else {
+        depSpin.start();
+        shell.exec(`cd ${foldername} && npm install`, () => {
+          depSpin.stop();
+          console.log("\n Npm packages got installed\n".green);
+          resolve();
+        });
+      }
     }
   });
 };
@@ -104,10 +132,17 @@ const open = () => {
     `.red
     );
     console.log("\nYou are all ready to go forth and conquer\n".trap);
-    if (shell.which("code"))
-      shell.exec(`cd ${app} && code .`, () => {
-        resolve();
-      });
+    if (shell.which("code")) {
+      if (!foldername) {
+        shell.exec(`cd ${app} && code .`, () => {
+          resolve();
+        });
+      } else {
+        shell.exec(`cd ${foldername} && code .`, () => {
+          resolve();
+        });
+      }
+    }
   });
 };
 
