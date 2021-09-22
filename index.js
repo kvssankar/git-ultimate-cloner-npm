a8698ca66877ce4ae5fd67057d3b3048228cecce
Kvs Sankar Kumar <55633494+kvssankar@users.noreply.github.com>
2021-09-22 16:30:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,32 +4,68 @@ const colors = require("colors");
 var Spinner = require("cli-spinner").Spinner;
 var depSpin = new Spinner("%s");
 depSpin.setSpinnerString("|/-\\");
+const fs = require("fs");
+
+let idename = "code";
+
+const help = () => {
+  shell.echo("\nFirst cd into the folder you want to clone\n");
+  shell.echo("\nUsage: quick clone <repo-link> <options>\n");
+  shell.echo("where options is one of the below\n");
+  shell.echo("-e atom   : for opening in atom editor");
+  shell.echo("-e vscode : for opening in vscode editor");
+};
+
+if (process.argv[2] == "--help") {
+  help();
+  shell.exit(200);
+}
 
 let url = process.argv[3];
-let foldername = process.argv[4];
-let app;
+let options = process.argv.slice(4);
 
-
-  temp_str = url.replace("https://github.com/","");
-  let t1,t2;
-  for (let i=0;i<temp_str.length;i++) {
-      if (temp_str[i]=="/") {
-          t1=i+1;
-          break;
-      }
+const optionsExc = () => {
+  for (var i = 0; i < options.length; i++) {
+    if (options[i] == "-e" && options[i + 1] == "atom") {
+      idename = options[i + 1];
+      i++;
+    }
   }
-  temp_str = temp_str.slice(t1);
-  temp_str.replace("/","");
-  app = temp_str;
+};
 
+if (process.argv[2] != "clone") {
+  console.log("Not a valid command. Please follow proper syntax\n".red);
+  console.log(
+    "Syntax : lit clone <url> [-e{editor name}] [-c{custom folder name}]\n"
+      .yellow
+  );
+  console.log("Supported editors : Visual Studio Code, Atom".yellow);
+  console.log("For Visual Studio Code, use code. For Atom, use atom\n".yellow);
+  shell.exit(1);
+}
+
+temp_str = url.replace("https://github.com/", "");
+let t1;
+for (let i = 0; i < temp_str.length; i++) {
+  if (temp_str[i] == "/") {
+    t1 = i + 1;
+    break;
+  }
+}
+temp_str = temp_str.slice(t1);
+temp_str.replace("/", "");
+temp_str = temp_str.slice(0, -4);
+shell.echo(temp_str);
+
+// getting github repo name
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-
 const run = async () => {
   await check();
+  await optionsExc();
   await clone();
   await cd();
   await open();
@@ -47,104 +83,68 @@ const check = () => {
         "\nSorry, this script requires git installed globally :(\n".red
       );
     }
-    if (!shell.which("code")) {
-      shell.echo("\nVS Code not installed\n".yellow);
-    }
     resolve();
   });
 };
-
-const clone = () => {
+const clone = async () => {
   return new Promise((resolve) => {
     shell.echo("\nAll checks passed, Let the fun begin\n".rainbow);
-    console.log(`\n ${capitalizeFirstLetter(app)} is being cloned...\n`.cyan);
-
-    if (!foldername) {
-      depSpin.start();
-      shell.exec(`git clone ${url}`, () => {
-        depSpin.stop();
-        console.log(
-          `\n\n ${capitalizeFirstLetter(app)} has been cloned successfully\n`
-            .green
-        );
+    console.log(
+      `\n ${capitalizeFirstLetter(temp_str)} is being cloned...\n`.cyan
+    );
+    depSpin.start();
+    shell.exec(`git clone ${url}`, () => {
+      depSpin.stop();
+      console.log(
+        `\n\n ${capitalizeFirstLetter(temp_str)} has been cloned successfully\n`
+          .green
+      );
       resolve();
-      });
-    } else {
-      depSpin.start();
-      shell.exec(`git clone ${url}`, () => {
-        depSpin.stop();
-        console.log(
-          `\n\n ${capitalizeFirstLetter(app)} has been cloned successfully\n`
-            .green
-        );
-      shell.exec(`mv ${app} ${foldername}`);
-      resolve();
-      });
-    }
+    });
   });
 };
 
 const cd = () => {
   return new Promise((resolve) => {
-    if (foldername) {
-      shell.cd(`${foldername}`);
-    } else {
-      shell.cd(`${app}`);
-    }
-      if (!shell.test("-f", "package.json")) {
+    shell.cd(`${temp_str}`);
+    if (!shell.test("-f", "package.json")) {
       console.log(`\n Can't find package.json in the root directory \n`.yellow);
       shell.cd("..");
       resolve();
     } else {
-      shell.cd("..");
       console.log("\n Npm packages are being installed...".yellow);
-
-      if (!foldername) {
-        depSpin.start();
-        shell.exec(`cd ${app} && npm install`, () => {
-          depSpin.stop();
-          console.log("\n Npm packages got installed\n".green);
-          resolve();
-        });
-      } else {
-        depSpin.start();
-        shell.exec(`cd ${foldername} && npm install`, () => {
-          depSpin.stop();
-          console.log("\n Npm packages got installed\n".green);
-          resolve();
-        });
-      }
+      depSpin.start();
+      shell.exec(`npm install`, () => {
+        depSpin.stop();
+        console.log("\n Npm packages got installed\n".green);
+        resolve();
+      });
     }
   });
 };
 
 const open = () => {
   return new Promise((resolve) => {
-    console.log(
-      `\n
-    .-----------------TTTT_-----___
-                     /''''''''''(___O] ----------_  \___/]
-  _...---'"""\ --''   Q                               _____@
-|'''                   ._   _____=---------"""""""
-|                ..--''|   l L |_l   |
-|          ..--''      .  /-___j '   '
-|    ..--''           /  ,       '   '
-|--''                /   /       '   '
-    `.red
-    );
     console.log("\nYou are all ready to go forth and conquer\n".trap);
-    if (shell.which("code")) {
-      if (!foldername) {
-        shell.exec(`cd ${app} && code .`, () => {
-          resolve();
-        });
-      } else {
-        shell.exec(`cd ${foldername} && code .`, () => {
-          resolve();
-        });
-      }
-    }
+    checkIde(idename);
+    shell.exec(`${idename} .`, () => {
+      resolve();
+    });
   });
+};
+
+const checkIde = (idename) => {
+  if (idename == "code") {
+    if (!shell.which("code")) {
+      shell.echo("\nError:VS Code not installed\n".red);
+      shell.exit(1);
+    }
+  } else {
+    if (!shell.which("atom")) {
+      shell.echo("\nError:Atom not installed\n".red);
+      shell.exit(1);
+    }
+  }
 };
 
 run();
